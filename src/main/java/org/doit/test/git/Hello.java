479b051bd7d1f82addeae738e51db0b19e5fb261
homevive <homevive@gmail.com>
2014-11-21 09:53:07 +0800

    Initial commit

---
+++ src/main/java/org/doit/test/git/Hello.java
@@ -0,0 +1,10 @@
+package org.doit.test.git;
+
+/**
+ * Created by jiajun on 11/21.
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    }
+}
